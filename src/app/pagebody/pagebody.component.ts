709a6734d40a0df1112778c9f65f882b247f46b9
sarvesh-0 <sarveshteware651@gmail.com>
2024-11-30 23:21:07 +0530

    Classwork1

---
+++ src/app/pagebody/pagebody.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pagebody',
+  imports: [],
+  templateUrl: './pagebody.component.html',
+  styleUrl: './pagebody.component.css'
+})
+export class PagebodyComponent {
+  
+}
